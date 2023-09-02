5f35beb4b04e80823c8e3a32d1816f4f4ecf994b
Heberth Santos <86444094+Alyah12@users.noreply.github.com>
2023-09-02 01:29:18 -0300

    Mudanças pontuais

--- avançando/notas.php
+++ avançando/notas.php
@@ -2,11 +2,37 @@
 
 $notas = [
     'Ana' => 10,
-    'João' => 8,
+    'João' => null,
     'Maria' => 9,
     'Miguel' => 7,
     'Samuel' => 6
 ];
 
-usort($notas);
+ksort($notas);
 var_dump($notas);
+
+// Função de ordenação de Arrays
+if (is_array($notas)) {
+    echo 'Sim, é um array' .PHP_EOL;
+}
+
+// Checa se é de fato uma array
+
+var_dump(
+    array_is_list($notas)
+);
+
+// Checa se a array está em formato de lista.
+
+echo 'Vinicius fez a prova ?' . PHP_EOL;
+var_dump(isset($notas['Ana']));
+
+// Verifica se a chave existe e não é nula.
+
+echo 'Alguém tirou 10 ?' . PHP_EOL;
+var_dump(in_array(10, $notas, true));
+
+// Verifica se o valor existe
+
+echo 'Quem tirou 10 ?' . PHP_EOL;
+var_dump(array_search(6, $notas, true));
