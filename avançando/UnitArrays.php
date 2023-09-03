db3b5e685382085c6434102aecc3013a0d1b40f7
Heberth Santos <86444094+Alyah12@users.noreply.github.com>
2023-09-03 16:34:17 -0300

    Merge da Array

--- avançando/UnitArrays.php
+++ avançando/UnitArrays.php
@@ -1,1 +1,19 @@
 <?php
+
+$alunos2023 = [
+    0 => 'Heberth',
+    1 => 'João',
+    2 => 'Roberto',
+    3 => 'Maria',
+    ];
+
+$novosAlunos = [
+    5 => 'Nicole',
+    6 => 'Thiago',
+    7 => 'Vanessa',
+    8 => 'Julia',
+];
+
+$alunos2024 = $alunos2023 + $novosAlunos;
+
+var_dump($alunos2024);
