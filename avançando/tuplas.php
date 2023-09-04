4090bfc2963b357cb030939780c396277d6bd055
Heberth Santos <86444094+Alyah12@users.noreply.github.com>
2023-09-03 23:57:47 -0300

    Função list2, mais detalhes

--- avançando/tuplas.php
+++ avançando/tuplas.php
@@ -1,7 +1,14 @@
 <?php
 
-$dados = ['Vinicius', 10, 24];
+$dados = [
+    'nome' => 'Vinicius',
+    'nota' => 10,
+    'idade' => 24,
+];
 
-[$nome, $nota, $idade] = $dados;
+//['nome' => $nome, 'nota' => $nota, 'idade' => $idade] = $dados;
 
+extract($dados);
 var_dump($nome, $nota, $idade);
+
+var_dump(compact('nome','nota', 'idade'));
